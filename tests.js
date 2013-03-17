c0bbc4f132ea53d4516f8af2d0120e74a5eafb34
Rahul Krishnan <rahulkrishnan@outlook.com>
2013-03-17 18:06:18 +0530

    Retrieve object completed

--- tests.js
+++ tests.js
@@ -48,7 +48,7 @@ module.exports.testQueryApi = testCase({
     }
 });
 
-module.exports.testCreateObject = testCase({
+module.exports.testCreateAndGetObject = testCase({
     test: function(test){
         console.log('=> Sending create object request .. ');
         api.createObject(
@@ -58,12 +58,22 @@ module.exports.testCreateObject = testCase({
             meta : { message : 'Test Message'}          
 
         },function(res){
-            console.log('=> Received response verifying .. ');
-            test.equal(res.success, 1);
-            test.done();
+            console.log('=> Received create obj response verifying .. ');
+            test.equal(res.success, 1);                        
+            console.log('=> Sending get object request .. ');
+            api.getObject(
+            {
+             obj_id : res.obj_id
+            },function(resp){
+                console.log('=> Received get response verifying .. ');
+                test.equal(resp.error, 0);                
+                test.done();
+            });
         });
     }
 });
+
+
 
 module.exports.testSimpleRequest = testCase({
     test: function(test){
